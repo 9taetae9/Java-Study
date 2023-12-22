1b9b4b3aeb75e4e00c9e030f2038b82139e8929d
TaeHyoun Kim <kth9903@gmail.com>
2023-12-22 19:13:59 +0900

    15장 string 쳅터 완료

--- src/StringCheck.java
+++ src/StringCheck.java
@@ -12,6 +12,9 @@ public class StringCheck {
         sample.checkMatch();
         sample.checkIndexOf();
         sample.checkLastIndexOf();
+        sample.checkTrim();
+        sample.checkReplace();
+        sample.checkFormat();
     }
 
     public void checkAddress(String[] addresses){
@@ -63,5 +66,50 @@ public class StringCheck {
         System.out.println(text.lastIndexOf('a',20)); //3
         System.out.println(text.lastIndexOf("a ",20)); //3
         System.out.println(text.lastIndexOf('z')); //-1
+    }
+
+    public void checkTrim(){
+        String[] strings = new String[]{
+                " a"," b ","    c","d    ","e    f", "    ",
+        };
+
+        for(String string : strings){
+            System.out.println("["+string+"]");
+            System.out.println("["+string.trim()+"]");
+        }
+
+        String text="   d ";
+        if(text!=null&&text.trim().length()>0){
+            System.out.println("OK");
+        }
+    }
+
+    public void checkReplace(){
+        String text = "The String class represents character strings.";
+        System.out.println(text.replace('s','z'));
+        System.out.println(text);
+        System.out.println(text.replace("trings","trike"));
+        System.out.println(text);
+        System.out.println(text.replaceAll(" ","|"));
+        System.out.println(text.replaceFirst(" ","|1|").replaceFirst(" ","|2|"));
+
+        String text2 = "이 문장의 공백을 순서대로 바꿉니다.";
+        int numberOfSpacesToReplace = 3; // 바꾸고 싶은 공백의 개수
+        System.out.println("text2 replace 이전 : "+ text2);
+        for (int i = 1; i <= numberOfSpacesToReplace; i++) {
+            text2 = text2.replaceFirst(" ", "|" + i + "|");
+        }
+
+        System.out.println("text2 replace 이후 : "+ text2);
+    }
+
+    public void checkFormat(){
+        String text="제 이름은 %s입니다. 지금까지 %d 권의 책을 썼고, "
+                +"하루에 %f %%의 시간을 책을 쓰는데 할애하고 있습니다.";
+
+        String realText = String.format(text,"이상민",7,10.5);
+        //String realText = String.format(text,"이상민",7); missingformatargumentexception
+        System.out.println(realText);
+
     }
 }
